dca1223f155c070a2572075d3aee2f9067a1d03d
Tommy Chow <tommyxchow@gmail.com>
2022-10-11 14:16:33 -0400

    Improve news card styling and behavior

--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -22,31 +22,31 @@ export default function NewsCard({
   });
 
   return (
-    <article className='p-4 bg-neutral-800 rounded-lg grid items-center sm:grid-cols-4 gap-4'>
-      <div className='relative col-span-1 h-40'>
-        <Image
-          className='object-cover rounded-md'
-          src={image}
-          alt='Thumbnail'
-          sizes='25vw'
-          fill
-          priority
-        />
-      </div>
+    <a href={url} target='_blank' rel='noreferrer'>
+      <article className='p-4 bg-neutral-800 rounded-lg grid items-center sm:grid-cols-4 gap-4 hover:bg-opacity-50 transition active:scale-95'>
+        <div className='relative col-span-1 h-40'>
+          <Image
+            className='object-cover rounded-md'
+            src={image}
+            alt='Thumbnail'
+            sizes='25vw'
+            fill
+            priority
+          />
+        </div>
 
-      <div className='sm:col-span-3 flex flex-col gap-2'>
-        <p className='text-xs text-neutral-300 font-medium'>{source}</p>
+        <div className='sm:col-span-3 flex flex-col gap-2'>
+          <p className='text-xs text-neutral-300 font-medium'>{source}</p>
 
-        <a href={url} target='_blank' rel='noreferrer'>
           <h3 className='text-lg font-semibold line-clamp-2'>{headline}</h3>
-        </a>
 
-        <p className='text-neutral-300 text-sm'>
-          {formattedTime} • {formattedDate}
-        </p>
+          <p className='text-neutral-300 text-sm mb-2'>
+            {formattedTime} • {formattedDate}
+          </p>
 
-        <p className='text-neutral-300 line-clamp-2 text-sm'>{summary}</p>
-      </div>
-    </article>
+          <p className='text-neutral-300 line-clamp-2 text-sm'>{summary}</p>
+        </div>
+      </article>
+    </a>
   );
 }
