b5092415db4e513331ad3e58db74bfb095a66134
Tommy Chow <tommyxchow@gmail.com>
2022-10-13 14:33:31 -0400

    Fix random semicolon at bottom of page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class' disableTransitionOnChange>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
