ae13b2ed5b179aca1f684f35ca4d4c47555d91a6
Tommy Chow <tommyxchow@gmail.com>
2022-10-13 01:34:11 -0400

    Make header sticky & add backdrop blur

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,12 @@ import TickerForm from './TickerForm';
 
 export default function Header() {
   return (
-    <header className='max-w-screen-md m-auto py-8 px-4 md:px-0'>
-      <div className='flex items-center gap-2'>
+    <header className='sticky inset-0 z-50 py-8 px-4 backdrop-blur md:px-0'>
+      <div className='m-auto flex max-w-screen-md items-center gap-2'>
         <Link href='/'>
           <a>
             <HiHome
-              className='active:scale-90 hover:text-neutral-400 transition'
+              className='transition hover:text-neutral-400 active:scale-90'
               size={28}
               title='Go home'
             />
